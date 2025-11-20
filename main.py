1323aaf8dada5e5cb72cdc73dcbb0397e30e89a0
Paco <pacocanovaca97@gmail.com>
2025-11-20 16:07:16 +0100

    Generación de funciones y acciones posibles (métodos dela clase Pet)

--- main.py
+++ main.py
@@ -15,11 +15,48 @@ class Pet:
     MIN_HAPPINESS = 0
     MIN_ENERGY = 0
 
+    def show_status(self):
+        print(f"Estado de {self.name}:")
+        print(f"❤️  Felicidad: {self.happiness}\n⚡ Energía: {self.energy}\n🍖 Hambre: {self.hunger}")
+
+    def eat(self):
+        print("🍗 Ñam Ñam Ñam")
+        self.energy -= 1
+        self.hunger -= 3
+        self.happiness += 1
+
+    def sleep(self):
+        print("😴 zzzzzz")
+        self.energy += 4
+        self.hunger += 1
+        self.happiness += 1
+    
+    def play(self):
+        print("🎾 ¡Yupiii!")
+        self.energy -= 2
+        self.hunger += 2
+        self.happiness += 2
+
+    def get_bored(self):
+        print("🥱 Qué aburrimiento...")
+        self.happiness -= 2
+        self.energy -= 1
+        self.hunger += 1
+
+
 continuation = True
 new_pet = Pet(name)
 
+
 while continuation:
-    print(name)
+    new_pet.show_status()
+
+
+    # ALEATORIEDAD DE ABURRIMIENTO DESPUÉS DE ELEGIR ACCIÓN Y APLICARLA
+    get_bored_probability = random.randint(1,5)
+    if get_bored_probability == 1:
+        new_pet.get_bored()
+
     continuation = False
 
 
